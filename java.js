0f03ca08f387cea2db24ad2a9bc6172947c6b5d5
nguyennamhai3465 <cakechief2347@gmail.com>
2022-11-06 21:08:34 +0700

    checkpoint

---
+++ java.js
@@ -0,0 +1,94 @@
+// console.log(vungs)
+// {
+//     {
+//     let vung2 = "Đây là vùng 2"
+//     }
+//     {
+//     console.log(vung2)
+
+//     }  
+//     }
+//if(7>10){
+//  console.log("Điều kiện này đúng")
+//}
+//  else {
+//    console.log("Điều kiện này sai")
+//}
+
+//let diem = Number(prompt("Mời bạn nhập điểm"));
+//console.log("diem: ",diem);
+
+//if (diem >= 8) {
+//    alert("Học sinh giỏi")
+//}
+//if (diem <  8 && diem > 6.5) {
+  //  alert("Học sinh Khá")
+//}
+//if (diem = 6.5) {
+ ///   alert("Học sinh trung bình")
+///}
+//if (diem <= 5 && diem >= 5) {
+  //  alert("Học sinh yếu")
+//}
+
+//let i = 1;
+//while(i < 10){
+  //  console.log(i);
+    //i = i + 1;
+//}
+
+//let i = 1
+//do{
+  //  console.log(i)
+    //i = i + 1
+//}
+//while (i <= 10);
+
+//for(let i = 1; i <= 10; i = i + 1){
+
+
+//console.log(i)
+//}
+
+//let arr = []
+//console.log(typeof(arr))
+//console.log(Array.isArray(arr))
+
+//arr.push("Phan tu 1",1234, "Phan tu thu 3")
+//console.log(Array(arr))
+//for(let i = 0;i < arr.length;i = i + 1){
+  //console.log(arr[i])
+//}
+
+//arr[1] = "Phan tu thu 2"
+/// array.splice (, ,)
+
+/*
+Buổi 3:
+- Function
+- Vòng lặp
+- Switch case
+- Duyệt mảng
+*/
+
+///// Hàm: 
+/// Khai báo hàm
+// C1: 
+// function tenHam(){
+//   /// code
+// }
+
+// // C2: 
+// const tenHam = function(){
+// }
+ // trac nghiem
+ //1.D
+ //2.B
+ //3.D
+ //4.C
+ //5.B
+ //6.A
+ //7.A
+ //8.D
+ //9.B
+ //10.c
